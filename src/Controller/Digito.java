4ab4215c1fbce21009a324f46b063f529ddf0b1c
T450 <T450@Mari.mshome.net>
2023-04-22 21:22:59 -0300

    at1.1

--- src/Controller/Digito.java
+++ src/Controller/Digito.java
@@ -9,7 +9,7 @@ public class Digito {
 		// se for entre o intervalo -10 e 10 retorna 1 , fiz para conseguir contabilizar o 0
 		if (num >-10 && num < 10) {
 				return 1;
-		}else {
+		}else {//aqui como não precisei do resto da divisão a própria recursiva já faz a divisão
 			return 1 + Qdig(num/10);
 		}
 	}
